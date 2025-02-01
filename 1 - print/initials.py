cc3696ecb8c8c1ad9c8f6fc6049fc3eb60508783
dudamakesthings <dudamakesthings@gmail.com>
2025-02-01 23:50:05 +0100

    Add folder for first chapter

--- 1 - print/initials.py
+++
@@ -1,9 +0,0 @@
-#Fun fact about me: I like crochet and knitting
-
-print("EEEEE  RRRR ")
-print("E      R   R")
-print("E      R   R")
-print("EEE    RRRR ")
-print("E      R R  ")
-print("E      R  R ")
-print("EEEEE  R   R")
