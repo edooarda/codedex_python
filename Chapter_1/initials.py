962a7e261dbeff0a05dc9374fd79a54950902ad1
dudamakesthings <dudamakesthings@gmail.com>
2025-02-01 23:48:16 +0100

    add print exercises from first chapter

---
+++ Chapter_1/initials.py
@@ -0,0 +1,9 @@
+#Fun fact about me: I like crochet and knitting
+
+print("EEEEE  RRRR ")
+print("E      R   R")
+print("E      R   R")
+print("EEE    RRRR ")
+print("E      R R  ")
+print("E      R  R ")
+print("EEEEE  R   R")
---
+++ Chapter_1/letter.py
@@ -0,0 +1,12 @@
+# Questions:
+# Today's date.
+# How you are feeling right now.
+# What you want to accomplish by learning to code.
+# A little message to your older, wiser, and programmer self.
+# Your favorite emoji to spice things up!
+
+print("First of February 2025")
+print("I am happy! 🐝")
+print("I want to help people")
+print("Don't stop to learn! Keep doing, you can do it! ❤️")
+print("🥳")
